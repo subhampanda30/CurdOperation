db3ee64c786b895b195a2519fb20c71b7fcce83a
Subham Panda <subhampanda.smaket@gmail.com>
2024-08-06 23:52:38 +0530

    Add project files.

---
+++ CurdOperation/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CurdOperation.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
